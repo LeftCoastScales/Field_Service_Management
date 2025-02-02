8f58e6b4660983d0bf4fb9c6c5b56593120d7202
realespee <wanyamasp@gmail.com>
2025-02-02 13:47:49 +0300

    Edit Service Order Workflow; Maintain 'Open' and 'Complete' States

--- beveren_fsm/field_service_management/doctype/service_appointment/service_appointment.py
+++ beveren_fsm/field_service_management/doctype/service_appointment/service_appointment.py
@@ -10,20 +10,9 @@ class ServiceAppointment(Document):
 		self.set_scheduled_status()
 	def validate(self):
 		self.set_scheduled_status()
-	def on_update_after_submit(self):
-		# self.update_service_order_status()
 		pass
 	def set_scheduled_status(self):
 		if self.status == "Open" and self.scheduled_start_datetime and self.scheduled_finish_datetime:
 			self.status = "Scheduled"
-	def update_service_order_status(self):
-		service_order = frappe.get_doc('Service Order', self.service_order)
-
-		# Update Service Order Status
-		if self.status == "Open": service_order.status = "Appointment"
-		if self.status == "Scheduled": service_order.status = "Scheduled"
-		if self.status == "In Progress": service_order.status = "In Progress"
-		if self.status == "Completed": service_order.status = "Completed"
-		service_order.save('Update')
-
+	
 		
