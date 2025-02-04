5ca6471834dbf00ad5dbe6576910ffc9b7ee1767
realespee <wanyamasp@gmail.com>
2025-02-04 15:28:49 +0300

    Implement Calculations for Taxes and Totals

--- beveren_fsm/field_service_management/doctype/service_order/service_order.js
+++ beveren_fsm/field_service_management/doctype/service_order/service_order.js
@@ -1,194 +1,134 @@
-// Copyright (c) 2025, Beveren Software and contributors
-// For license information, please see license.txt
+frappe.provide('beveren_fsm.field_management_system');
 
-frappe.ui.form.on("Service Order", {
-	refresh(frm) {
-		frm.trigger('filter_services');
-		frm.trigger('filter_parts');
+beveren_fsm.field_management_system.ServiceOrder = class ServiceOrder {
+    constructor(opts) {
+        $.extend(this, opts);
+        this.setup();
+    }
 
-		if (frm.doc.docstatus == 1) {
-			frm.trigger('add_actions_button');
-		}
-	},
-	filter_services: frm => {
-		frm.fields_dict["services"].grid.get_field("item_code").get_query = function(doc, cdt, cdn) {
-            return {
-                filters: {
-                    "item_group": "Services"
+    setup() {
+        this.items_field = "items";
+        this.tax_field = "taxes";
+        this.bind_events();
+    }
+
+    bind_events() {
+        // Bind to item table changes
+        frappe.ui.form.on(this.frm.doctype + " Item", {
+            items_add: () => this.calculate_totals(),
+            items_remove: () => this.calculate_totals(),
+            qty: () => this.calculate_totals(),
+            rate: () => this.calculate_totals(),
+            item_code: () => this.calculate_totals()
+        });
+
+        // Bind to tax table changes
+        frappe.ui.form.on(this.frm.doctype + " Tax", {
+            taxes_add: () => this.calculate_totals(),
+            taxes_remove: () => this.calculate_totals(),
+            rate: () => this.calculate_totals(),
+            tax_amount: () => this.calculate_totals()
+        });
+    }
+
+    calculate_totals() {
+        this.calculate_item_values();
+        this.calculate_taxes();
+        this.calculate_grand_total();
+        this.frm.refresh_fields();
+    }
+
+    calculate_item_values() {
+        const items = this.frm.doc[this.items_field] || [];
+        let total = 0;
+
+        items.forEach(item => {
+            item.amount = flt(item.qty * item.rate);
+            item.net_amount = item.amount;
+            total += item.amount;
+        });
+
+        this.frm.doc.total = total;
+        this.frm.doc.net_total = total;
+    }
+
+    calculate_taxes() {
+        const me = this;
+        const items = this.frm.doc[this.items_field] || [];
+        const taxes = this.frm.doc[this.tax_field] || [];
+
+        // Reset tax amounts
+        taxes.forEach(tax => {
+            tax.tax_amount = 0;
+            tax.total = 0;
+        });
+
+        // Calculate tax for each item
+        items.forEach(item => {
+            taxes.forEach(tax => {
+                if (tax.charge_type === "On Net Total") {
+                    tax.tax_amount += (item.net_amount * tax.rate) / 100;
                 }
-            };
-        };
-	},
-	filter_parts: frm => {
-		frm.fields_dict["parts"].grid.get_field("item_code").get_query = function(doc, cdt, cdn) {
-            return {
-                filters: {
-                    "item_group": "Products"
+            });
+        });
+
+        // Update tax totals
+        let grand_total = this.frm.doc.net_total;
+        taxes.forEach(tax => {
+            tax.total = grand_total;
+            grand_total += tax.tax_amount;
+        });
+    }
+
+    calculate_grand_total() {
+        const taxes = this.frm.doc[this.tax_field] || [];
+        let grand_total = this.frm.doc.net_total;
+        let total_taxes = 0;
+
+        taxes.forEach(tax => {
+            total_taxes += tax.tax_amount;
+        });
+
+        this.frm.doc.total_taxes = total_taxes;
+        this.frm.doc.grand_total = grand_total + total_taxes;
+        this.frm.doc.rounded_total = Math.round(this.frm.doc.grand_total);
+    }
+}
+
+// Form Controller
+frappe.ui.form.on('Service Order', {
+    setup(frm) {
+        frm.service_order = new beveren_fsm.field_management_system.ServiceOrder({frm: frm});
+    },
+
+    refresh(frm) {
+        frm.service_order.calculate_totals();
+    },
+
+    validate(frm) {
+        frm.service_order.calculate_totals();
+    },
+	// Add new handler for taxes_and_charges
+    taxes_and_charges: function(frm) {
+        if(frm.doc.taxes_and_charges) {
+            frappe.call({
+                method: 'erpnext.controllers.accounts_controller.get_taxes_and_charges',
+                args: {
+                    "master_doctype": "Sales Taxes and Charges Template",
+                    "master_name": frm.doc.taxes_and_charges
+                },
+                callback: function(r) {
+                    if(!r.exc) {
+                        frm.clear_table("taxes");
+                        for(var i=0; i<r.message.length; i++) {
+                            var d = frm.add_child("taxes");
+                            $.extend(d, r.message[i]);
+                        }
+                        frm.refresh_field("taxes");
+                        frm.service_order.calculate_totals();
+                    }
                 }
-            };
-        };
-	},
-	add_actions_button: frm => {
-		frm.page.add_action_item(__(' Create Appointment'), function() {
-			frm.trigger('create_appointment');
-		});
-
-		frm.page.add_action_item(__('Create Invoice'), function() {
-			frm.trigger('create_service_invoice');
-		});
-	},
-	create_appointment: frm => {
-		if(frm.doc.docstatus == 1) {
-			frappe.call({
-				method: "create_appointment",
-				doc: frm.doc,
-				args: {
-					service_order: frm.doc.name,
-				},
-				callback: function(r) {
-					if (r.message) {
-						route_options = {"source": "Service Order"};
-						frappe.set_route("Form", "Service Appointment", r.message);
-					}
-				}
-			})
-		}
-	},
-	create_service_invoice: frm => {
-		// Fetch all parts and services which are not invoiced
-		let parts = frm.doc.parts || [];
-		let services = frm.doc.services || [];
-		let non_invoiced_items = [];
-		parts.forEach(item => {
-			if (item.invoice_status != 'Invoiced') {
-				non_invoiced_items.push(item);
-			}
-		});
-		services.forEach(service => {
-			if (service.invoice_status != 'Invoiced') {
-				non_invoiced_items.push(service);
-			}
-		});
-		if (non_invoiced_items.length == 0) {
-			frappe.msgprint("This Appointment is already fully Invoiced.");
-			return;
-		}
-		function mergeDuplicates(items) {
-			let mergedItems = items.reduce((acc, item) => {
-				let existingItem = acc.find(i => i.item_code === item.item_code);
-				if (existingItem) {
-					existingItem.qty += item.qty; 
-				} else {
-					acc.push({ ...item }); 
-				}
-				return acc;
-			}, []);
-		
-			return mergedItems;
-		}
-		non_invoiced_items = mergeDuplicates(non_invoiced_items);
-		
-		const dialog = new frappe.ui.Dialog({
-			title: __("Services and Parts to Invoice"),
-			fields: [
-				{
-					fieldname: "service_items",
-					fieldtype: "Table",
-					label: __("Services and Parts"),
-					options: "Services and Parts",
-					in_place_edit: true,
-					reqd: 1,
-					fields: [
-						{
-							fieldname: "item_code",
-							label: __("Item Code"),
-							fieldtype: "Link",
-							options: "Item",
-							in_list_view: 1,
-						},
-						{
-							fieldname: "item_name",
-							label: __("Item Name"),
-							fieldtype: "Data",
-							in_list_view: 1,
-						},
-						{
-							fieldname: "qty",
-							label: __("Quantity"),
-							fieldtype: "Int",
-							in_list_view: 1,
-						},
-						{
-							fieldname: "amount",
-							label: __("Amount"),
-							fieldtype: "Currency",
-							in_list_view: 1,
-						}
-					]
-				}
-			],
-			primary_action: (values) => {
-				// Create Invoice
-				frappe.call({
-					method: "beveren_fsm.field_service_management.fsm_utils.create_service_invoice",
-					args: {
-						doctype: frm.doc.doctype,
-						docname: frm.doc.name,
-						customer: frm.doc.customer,
-						items: values.service_items
-					},
-					callback: function(r) {
-						if (r.message) {
-							// Route to Sales Invoice
-							frappe.set_route("Form", "Sales Invoice", r.message);
-						}
-					}
-				})
-				
-				dialog.hide();
-			},
-			primary_action_label: __("Create Invoice"),
-			secondary_action: () => { dialog.hide(); }, 
-			secondary_action_label: __("Cancel"),
-				});
-			
-
-			// Prefill the table with non-invoiced items
-			let tableField = dialog.get_field("service_items");
-			tableField.df.data = non_invoiced_items;
-			tableField.grid.refresh();
-
-			dialog.show();
-	},
-	
+            });
+        }
+    }
 });
-
-
-frappe.ui.form.on("Service Order Item", {
-	item_code: function (frm, cdt, cdn) {
-		var row = locals[cdt][cdn];
-		if (frm.doc.delivery_date) {
-			row.delivery_date = frm.doc.delivery_date;
-			refresh_field("delivery_date", cdn, "items");
-		} else {
-			frm.script_manager.copy_from_first_row("items", row, ["delivery_date"]);
-		}
-	},
-	delivery_date: function (frm, cdt, cdn) {
-		if (!frm.doc.delivery_date) {
-			erpnext.utils.copy_value_in_all_rows(frm.doc, cdt, cdn, "items", "delivery_date");
-		}
-	},
-	rate: function(frm, cdt, cdn) {		
-        var row = locals[cdt][cdn];
-		var amount = row.rate * row.qty;
-		frappe.model.set_value(cdt, cdn, "amount", amount);
-    },
-    qty: function(frm, cdt, cdn) {
-        var row = locals[cdt][cdn];
-		var amount = row.rate * row.qty;
-		frappe.model.set_value(cdt, cdn, "amount", amount);
-    },
-	
-});
-
--- beveren_fsm/field_service_management/doctype/service_order/service_order.py
+++ beveren_fsm/field_service_management/doctype/service_order/service_order.py
@@ -2,42 +2,107 @@
 # For license information, please see license.txt
 
 import frappe
+from frappe import _
+from frappe.utils import flt
 from frappe.model.document import Document
+from erpnext.controllers.accounts_controller import AccountsController
 
-
-class ServiceOrder(Document):
-	@frappe.whitelist()
-	def create_appointment(self, service_order):
-		appointment = frappe.new_doc("Service Appointment")
-		appointment.service_order = service_order
-		appointment.customer = self.customer
-
-		for service in self.services:  
-			appointment.append("services", {
-				"item_code": service.item_code,
-				"qty": service.qty, 
-				"rate": service.rate,
-				"amount": service.amount,
-				"invoice_status": service.invoice_status
-			})
-		for parts in self.parts:  
-			appointment.append("parts", {
-				"item_code": parts.item_code,
-				"qty": parts.qty,  
-				"rate": parts.rate,
-				"amount": parts.amount,
-				"invoice_status": parts.invoice_status
-			})
+class ServiceOrder(AccountsController):
+	def __init__(self, *args, **kwargs):
+		super(ServiceOrder, self).__init__(*args, **kwargs)
+		self._company_currency = None
 		
-		self.update_status()
-		appointment.insert()
+	@property
+	def company_currency(self):
+		if not self._company_currency:
+			self._company_currency = frappe.get_cached_value('Company', self.company, "default_currency")
+		return self._company_currency
+		
+	def validate(self):
+		self.validate_currency()
+		self.validate_items()
+		self.calculate_totals()
+		self.calculate_taxes()
+		self.calculate_base_amounts()
+		self.set_in_words()
+	
+	@property
+	def company_currency(self):
+		if not self._company_currency:
+			self._company_currency = frappe.get_cached_value('Company', self.company, "default_currency")
+		return self._company_currency
 
+	def validate_currency(self):
+		from erpnext.controllers.accounts_controller import validate_conversion_rate
+		if not self.currency:
+			self.currency = self.company_currency
 
-		return appointment.name
+		# Set conversion rate to 1 if same currency
+		if self.currency == self.company_currency and not self.conversion_rate:
+			self.conversion_rate = 1.0
+			
+		if not self.conversion_rate:
+			self.conversion_rate = get_exchange_rate(self.currency, self.company_currency, self.posting_date)
+			
+		validate_conversion_rate(
+			conversion_rate=self.conversion_rate,
+			conversion_rate_label="Conversion Rate",
+			company=self.company,
+			currency=self.currency
+		)
+		
+		
+	def validate_items(self):
+		for item in self.get('items'):
+			item.base_rate = flt(item.rate * self.conversion_rate, self.precision("base_rate", item))
+			item.base_amount = flt(item.base_rate * item.qty, self.precision("base_amount", item))
+			item.net_amount = item.amount
+			item.base_net_amount = item.base_amount
+			
+	def calculate_totals(self):
+		self.total_qty = 0
+		self.total = 0
+		self.base_total = 0
+		
+		for item in self.get('items'):
+			self.total_qty += flt(item.qty)
+			self.total += flt(item.amount)
+			self.base_total += flt(item.base_amount)
+			
+		self.net_total = self.total
+		self.base_net_total = self.base_total
 
-	def update_status(self):
-		self.status = "Appointment"
-
-	def create_invoice(self):
-		#TODO: Implement this
-		pass
+	def calculate_base_amounts(self):
+		self.base_total = flt(self.total * self.conversion_rate)
+		self.base_net_total = flt(self.net_total * self.conversion_rate)
+		self.base_total_taxes_and_charges = flt(self.total_taxes_and_charges * self.conversion_rate)
+		self.base_grand_total = flt(self.grand_total * self.conversion_rate)
+		self.base_rounding_adjustment = flt(self.rounding_adjustment * self.conversion_rate)
+		self.base_rounded_total = flt(self.base_grand_total + self.base_rounding_adjustment)
+	
+	def set_in_words(self):
+		from frappe.utils import money_in_words
+		self.in_words = money_in_words(self.grand_total, self.currency)
+		self.base_in_words = money_in_words(self.base_grand_total, self.company_currency)
+		
+	def calculate_taxes(self):
+		self.total_taxes_and_charges = 0
+		self.base_total_taxes_and_charges = 0
+		
+		if self.get('taxes'):
+			for tax in self.taxes:
+				if tax.charge_type == "On Net Total":
+					tax.tax_amount = flt(self.net_total * flt(tax.rate) / 100)
+					tax.base_tax_amount = flt(tax.tax_amount * self.conversion_rate)
+					
+					self.total_taxes_and_charges += tax.tax_amount
+					self.base_total_taxes_and_charges += tax.base_tax_amount
+					
+		self.grand_total = flt(self.net_total + self.total_taxes_and_charges)
+		self.base_grand_total = flt(self.base_net_total + self.base_total_taxes_and_charges)
+		
+		self.rounded_total = round(self.grand_total)
+		self.base_rounded_total = round(self.base_grand_total)
+		
+		self.rounding_adjustment = flt(self.rounded_total - self.grand_total)
+		self.base_rounding_adjustment = flt(self.base_rounded_total - self.base_grand_total)
