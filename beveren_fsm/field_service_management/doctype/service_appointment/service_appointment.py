bc11427df9fb428a1524d660c5e1f777275f4142
realespee <wanyamasp@gmail.com>
2025-02-17 19:26:10 +0300

    Make Schedule/Dispatch Page and Dependencies

--- beveren_fsm/field_service_management/doctype/service_appointment/service_appointment.py
+++ beveren_fsm/field_service_management/doctype/service_appointment/service_appointment.py
@@ -6,13 +6,15 @@ from frappe.model.document import Document
 
 
 class ServiceAppointment(Document):
-	def before_save(self):
-		self.set_scheduled_status()
-	def validate(self):
-		self.set_scheduled_status()
-		pass
-	def set_scheduled_status(self):
-		if self.status == "Open" and self.scheduled_start_datetime and self.scheduled_finish_datetime:
-			self.status = "Scheduled"
-	
-		
+    def before_save(self):
+        self.set_scheduled_status()
+
+    def validate(self):
+        self.set_scheduled_status()
+
+    def set_scheduled_status(self):
+        if self.scheduled_start_datetime and self.scheduled_finish_datetime:
+            if self.get("service_technicians") and len(self.get("service_technicians")) > 0:
+                self.status = "Dispatched"
+            elif self.status == "Open":
+                self.status = "Scheduled"
